eaec25de4c79ef2fa27094bed08baab43dd18e52
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-11-08 13:58:58 +0100

    add LinkedIn link for Dominik and update description for Andrija

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -9,13 +9,14 @@ export const teamMembers = [
     about:
       "With over 9 years in the IT industry and a Master’s in Software Engineering, I've specialized as a Senior Software Developer and Team Lead in mobile application development. My expertise lies in creating sophisticated software architectures and writing clean code, with a focus on exceptional user experiences.",
     githubLink: "https://github.com/dkoscica",
-    linkedinLink: "",
+    linkedinLink: "https://www.linkedin.com/in/dominikkoscica/",
   },
   {
     id: 2,
     name: "Adrian Bićanić",
     title: "Senior Front-End Engineer",
-    description: "Building apps with JavaScript, TypeScript, and React.",
+    description:
+      "Building apps with JavaScript, TypeScript, React and other web technologies.",
     imgSrc: "/members/adrian-bicanic.jpeg",
     about:
       "I am a Senior Front-End Engineer with a passion for building web applications using JavaScript, TypeScript, and React. I have a strong background in software development and a Bachelor’s degree in Computer Science. My expertise includes creating responsive and user-friendly interfaces, optimizing performance, and ensuring high-quality code. I am dedicated to continuous learning and staying up-to-date with the latest technologies and best practices in the industry. I am also interested in design, photography, and music. In my free time, I enjoy playing the guitar, taking photos, and exploring new places. I am always looking for new challenges and opportunities to grow as a developer and contribute to exciting projects. Feel free to connect with me on LinkedIn to discuss collaboration, projects, or anything related to web development and technology.",
@@ -26,7 +27,8 @@ export const teamMembers = [
     id: 3,
     name: "Andrija Kapetanović",
     title: "Front-End Developer",
-    description: "Ensuring smooth project execution and client satisfaction.",
+    description:
+      "Crafting user-friendly web applications with attention to detail and practical design.",
     imgSrc: "/members/andrija-kapetanovic.jpg",
     about:
       "I am a Front-End Developer with a passion for creating user-friendly web applications. I specialize in front-end technologies such as HTML, CSS, and JavaScript, with a focus on React and Next.js. I have a Bachelor’s degree in Computer Science and over 5 years of experience in software development. My expertise includes building responsive and accessible interfaces, optimizing performance, and ensuring a seamless user experience. ",
