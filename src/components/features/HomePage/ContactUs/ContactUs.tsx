2bd9177d4285b7dd06ebbb25874bc089c81eb3a3
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-11-25 15:45:59 +0100

    remove PageContainer from children components and add it to the parent, Home component

--- src/components/features/HomePage/ContactUs/ContactUs.tsx
+++ src/components/features/HomePage/ContactUs/ContactUs.tsx
@@ -1,32 +1,29 @@
 import { Section } from "@/components/core/Section/Section";
 import { Text } from "@/components/core/Text/Text";
 import { ContactForm } from "./ContactForm";
-import { PageContainer } from "@/components/shared/PageContainer";
 import { Card } from "@/components/core/Card/Card";
 
 export const ContactUs = () => {
   return (
-    <Section py="16" bg="white">
-      <PageContainer>
-        <Card
-          border="1px solid"
-          maxW="md"
-          mx="auto"
-          rounded="lg"
-          shadow="md"
-          p={{ base: "4", sm: "8" }}
+    <Section pt="16" pb="14" mb="2" bg="white">
+      <Card
+        border="1px solid"
+        maxW="md"
+        mx="auto"
+        rounded="lg"
+        shadow="md"
+        p={{ base: "4", sm: "8" }}
+      >
+        <Text
+          as="h2"
+          fontSize={{ base: "3xl", md: "4xl" }}
+          textAlign="center"
+          mb="6"
         >
-          <Text
-            as="h2"
-            fontSize={{ base: "3xl", md: "4xl" }}
-            textAlign="center"
-            mb="6"
-          >
-            Contact us
-          </Text>
-          <ContactForm />
-        </Card>
-      </PageContainer>
+          Contact us
+        </Text>
+        <ContactForm />
+      </Card>
     </Section>
   );
 };
--- src/components/features/HomePage/Home.tsx
+++ src/components/features/HomePage/Home.tsx
@@ -1,13 +1,16 @@
 import { Hero } from "./Hero";
 import { Members } from "./Members/Members";
 import { ContactUs } from "./ContactUs/ContactUs";
+import { TechStack } from "./TechStack/TechStack";
+import { PageContainer } from "@/components/shared/PageContainer";
 
 export const Home = () => {
   return (
-    <>
+    <PageContainer>
       <Hero />
       <Members />
+      <TechStack />
       <ContactUs />
-    </>
+    </PageContainer>
   );
 };
