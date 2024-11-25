f2711dbf984c2b63bbe4a04fdb8cadcbb712cb7d
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-11-25 15:46:09 +0100

    create TechStack

--- panda.config.ts
+++ panda.config.ts
@@ -49,47 +49,6 @@ export const zIndex = {
   modal: { value: 9000 },
 } as const;
 
-export const keyframes = {
-  enter: {
-    from: {
-      opacity: "var(--enter-opacity, 1)",
-      transform:
-        "translate3d(var(--enter-translate-x, 0), var(--enter-translate-y, 0), 0) scale3d(var(--enter-scale, 1), var(--enter-scale, 1), var(--enter-scale, 1)) rotate(var(--enter-rotate, 0))",
-    },
-  },
-  exit: {
-    to: {
-      opacity: "var(--exit-opacity, 1)",
-      transform:
-        "translate3d(var(--exit-translate-x, 0), var(--exit-translate-y, 0), 0) scale3d(var(--exit-scale, 1), var(--exit-scale, 1), var(--exit-scale, 1)) rotate(var(--exit-rotate, 0))",
-    },
-  },
-  fadeIn: {
-    "0%": { opacity: 0 },
-    "100%": { opacity: 1 },
-  },
-  fadeOut: {
-    "0%": { opacity: 1 },
-    "100%": { opacity: 0 },
-  },
-  slideInFromTop: {
-    "0%": { transform: "translateY(-100%)" },
-    "100%": { transform: "translateY(0)" },
-  },
-  slideInFromBottom: {
-    "0%": { transform: "translateY(1000%)" },
-    "100%": { transform: "translateY(0)" },
-  },
-  slideOutToRight: {
-    "0%": { transform: "translateX(0)" },
-    "100%": { transform: "translateX(120%)" },
-  },
-  spin: {
-    "0%": { transform: "rotate(0deg)" },
-    "100%": { transform: "rotate(360deg)" },
-  },
-} as const;
-
 export const colors = {
   brand: {
     50: { value: "{colors.blue.100}" },
@@ -158,6 +117,58 @@ export const colors = {
   },
 } as const;
 
+export const gradients = {
+  "side-fog": {
+    value:
+      "linear-gradient(to right, transparent 0, #000 128px, #000 calc(100%-128px), transparent 100%)",
+  },
+} as const;
+
+export const keyframes = {
+  enter: {
+    from: {
+      opacity: "var(--enter-opacity, 1)",
+      transform:
+        "translate3d(var(--enter-translate-x, 0), var(--enter-translate-y, 0), 0) scale3d(var(--enter-scale, 1), var(--enter-scale, 1), var(--enter-scale, 1)) rotate(var(--enter-rotate, 0))",
+    },
+  },
+  exit: {
+    to: {
+      opacity: "var(--exit-opacity, 1)",
+      transform:
+        "translate3d(var(--exit-translate-x, 0), var(--exit-translate-y, 0), 0) scale3d(var(--exit-scale, 1), var(--exit-scale, 1), var(--exit-scale, 1)) rotate(var(--exit-rotate, 0))",
+    },
+  },
+  fadeIn: {
+    "0%": { opacity: 0 },
+    "100%": { opacity: 1 },
+  },
+  fadeOut: {
+    "0%": { opacity: 1 },
+    "100%": { opacity: 0 },
+  },
+  slideInFromTop: {
+    "0%": { transform: "translateY(-100%)" },
+    "100%": { transform: "translateY(0)" },
+  },
+  slideInFromBottom: {
+    "0%": { transform: "translateY(1000%)" },
+    "100%": { transform: "translateY(0)" },
+  },
+  slideOutToRight: {
+    "0%": { transform: "translateX(0)" },
+    "100%": { transform: "translateX(120%)" },
+  },
+  spin: {
+    "0%": { transform: "rotate(0deg)" },
+    "100%": { transform: "rotate(360deg)" },
+  },
+  "infinite-scroll": {
+    "0%": { transform: "translateX(0)" },
+    "100%": { transform: "translateX(-100%)" },
+  },
+} as const;
+
 export const animations = {
   enter: { value: "enter 0.5s var(--ease-out)" },
   exit: { value: "exit 0.5s var(--elastic-out)" },
@@ -167,6 +178,7 @@ export const animations = {
   slideInFromBottom: { value: "slideInFromBottom 0.6s var(--ease-out)" },
   slideOutToRight: { value: "slideOutToRight 0.9s var(--elastic-out)" },
   spin: { value: "spin 1s linear infinite" },
+  "infinite-scroll": { value: "infinite-scroll 50s linear infinite" },
 } as const;
 
 export default defineConfig({
@@ -194,6 +206,7 @@ export default defineConfig({
         shadows,
         zIndex,
         animations,
+        gradients,
       },
       semanticTokens: {
         colors,
