bf5df2f2d3f4390cdc03e0ee8528501d8e8f5aec
Dominik Koscica <dominik.koscica@Dominiks-MacBook-Pro-2.local>
2025-01-12 13:12:12 +0100

    Update next.config.mjs basePath

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   // Recommended: this will reduce output
   // Docker image size by 80%+
   output: "export",
+  basePath: '/syncity-web',
   // Optional: bring your own cache handler
   // cacheHandler: path.resolve('./cache-handler.mjs'),
   // cacheMaxMemorySize: 0, // Disable default in-memory caching
