5ce151e5cc34bcb5bbcac022fb170fe25566f05e
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-05-03 00:12:47 +0200

    refactor: use RouteLink in Logo for consistency

--- src/components/core/Logo/Logo.tsx
+++ src/components/core/Logo/Logo.tsx
@@ -1,12 +1,9 @@
-import { Link } from "@tanstack/react-router";
-import { styled } from "@/styled-system/jsx";
-
-const StyledLink = styled(Link);
+import { RouteLink } from "@/components/core/RouteLink/RouteLink";
 
 export const Logo = ({ width = 65, height = 65 }) => {
   return (
-    <StyledLink to="/" rounded="full" outlineColor="brand.600">
+    <RouteLink to="/" rounded="full" outlineColor="brand.600">
       <img src="/Logo.png" width={width} height={height} alt="Syncity Logo" />
-    </StyledLink>
+    </RouteLink>
   );
 };
