d78e5ecc164f299654dca64ca3eafc40ada0c887
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-05-04 22:12:40 +0200

    feat(theme): add Park UI color palettes and light/dark conditions

---
+++ src/theme/colors/index.ts
@@ -0,0 +1,10 @@
+import { amber } from "./amber";
+import { blue } from "./blue";
+import { green } from "./green";
+import { red } from "./red";
+import { sky } from "./sky";
+import { slate } from "./slate";
+
+export { amber, blue, green, red, sky, slate };
+
+export const colorPalettes = { amber, blue, green, red, sky, slate };
