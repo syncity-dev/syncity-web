647d6b030199cd7d5d0da33047aaba1e88419130
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-05-07 17:35:45 +0200

    refactor(card): use ark factory elements for slot components

--- src/components/core/Card/Card.tsx
+++ src/components/core/Card/Card.tsx
@@ -1,3 +1,4 @@
+import { ark } from "@ark-ui/react/factory";
 import type { ComponentProps } from "react";
 import { createStyleContext } from "@/styled-system/jsx";
 import { card } from "@/styled-system/recipes";
@@ -5,9 +6,9 @@ import { card } from "@/styled-system/recipes";
 const { withProvider, withContext } = createStyleContext(card);
 
 export type CardRootProps = ComponentProps<typeof CardRoot>;
-export const CardRoot = withProvider("div", "root");
-export const CardHeader = withContext("div", "header");
-export const CardBody = withContext("div", "body");
-export const CardFooter = withContext("h3", "footer");
-export const CardTitle = withContext("h3", "title");
-export const CardDescription = withContext("div", "description");
+export const CardRoot = withProvider(ark.div, "root");
+export const CardHeader = withContext(ark.div, "header");
+export const CardBody = withContext(ark.div, "body");
+export const CardFooter = withContext(ark.div, "footer");
+export const CardTitle = withContext(ark.h3, "title");
+export const CardDescription = withContext(ark.div, "description");
